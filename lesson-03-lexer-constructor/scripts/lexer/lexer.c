02e599f231662f938d26e817d46975c20779b37c
SperenzaNarra <jerry_wang.sh@hotmail.com>
2023-02-23 15:48:53 +0800

    correct debug problem

--- lesson-03-lexer-constructor/scripts/lexer/lexer.c
+++ lesson-03-lexer-constructor/scripts/lexer/lexer.c
@@ -5,6 +5,5 @@
 int lex(struct lex_process* process)
 {
     struct logger* logger = get_logger("lexer.c", "lex");
-    logger->debug(logger, "lex() is called\n");
     return LEXICAL_ANALYSIS_ALL_OK;
 }
