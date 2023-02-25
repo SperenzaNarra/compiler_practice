652915e83474d87351d803ea8b05a0e7a2a49020
SperenzaNarra <jerry_wang.sh@hotmail.com>
2023-02-25 14:18:15 +0800

    Initial Commit

---
+++ lesson-06-lexer-operator/scripts/token/token.c
@@ -0,0 +1,71 @@
+#include "token.h"
+
+#include <string.h>
+#include "helpers/logger.h"
+
+
+static inline void read_pos(struct pos* pos)
+{
+    struct logger* logger = get_logger("token.c", "read_pos");
+    logger->debug(logger, "read pos\n");
+    logger->info(logger, "  col  : %d\n", pos->col);
+    logger->info(logger, "  line : %d\n", pos->line);
+    logger->info(logger, "  filename : %s\n", pos->filename);
+}
+
+static inline void read_token(struct token* token)
+{
+    struct logger* logger = get_logger("token.c", "read_token");
+    logger->debug(logger, "read token\n");
+
+    switch (token->type)
+    {
+    case TOKEN_TYPE_IDENTIFIER:
+        logger->info(logger, "  type  : identifier\n");
+        logger->info(logger, "  value : %s\n", token->sval);
+        break;
+    case TOKEN_TYPE_KEYWORD:
+        logger->info(logger, "  type  : keyword\n");
+        logger->info(logger, "  value : %s\n", token->sval);
+        break;
+    case TOKEN_TYPE_OPERATOR:
+        logger->info(logger, "  type  : operator\n");
+        logger->info(logger, "  value : %s\n", token->sval);
+        break;
+    case TOKEN_TYPE_SYMBOL:
+        logger->info(logger, "  type  : symbol\n");
+        logger->info(logger, "  value : %c\n", token->cval);
+        break;
+    case TOKEN_TYPE_NUMBER:
+        logger->info(logger, "  type  : number\n");
+        logger->info(logger, "  value : %lld\n", token->llnum);
+        break;
+    case TOKEN_TYPE_STRING:
+        logger->info(logger, "  type  : string\n");
+        logger->info(logger, "  value : %s\n", token->sval);
+        break;
+    case TOKEN_TYPE_COMMENT:
+        logger->info(logger, "  type  : comment\n");
+        logger->info(logger, "  value : %s\n", token->sval);
+        break;
+    case TOKEN_TYPE_NEWLINE:
+        logger->info(logger, "  type  : new line");
+        break;
+    };
+    logger->info(logger, "  whitespace : %s\n", token->whitespace ? "true" : "false");
+    logger->info(logger, "  between_brackets : %s\n", token->between_brackets);
+    read_pos(&token->pos);
+}
+
+
+struct token tmp_token;
+
+struct token* token_create(struct token* _token)
+{
+    struct logger* logger = get_logger("token.c", "token_create");
+
+    memcpy(&tmp_token, _token, sizeof(struct token));
+    read_token(&tmp_token);
+
+    return &tmp_token;
+}
